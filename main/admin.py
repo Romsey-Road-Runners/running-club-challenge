cda9bff768140c3e15bbc93e4ef159d295bad8a7
Sam Weston <sam.weston@tails.com>
2022-09-03 12:32:16 +0100

    Fix admin pages for new model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Athlete, Event, Race, Activity
+from .models import Athlete, Event, Race, Activity, EventTeam, EventTeamAthlete
 
 
 class AthleteAdmin(admin.ModelAdmin):
@@ -11,5 +11,7 @@ admin.site.register(Athlete, AthleteAdmin)
 admin.site.register(Event)
 admin.site.register(Race)
 admin.site.register(Activity)
+admin.site.register(EventTeam)
+admin.site.register(EventTeamAthlete)
 admin.site.site_header = "RRR Challenge administration"
 admin.site.site_title = "RRR Challenge admin"
